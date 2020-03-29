72c910a9e32b0fb32082ff722a53c49a699fb915
stdmatt <stdmatt@pixelwizards.io>
2020-03-29 18:10:06 +0300

    Make some changes on the olc game engine to compile both on GNU/Linux and windows

--- game/olc.cpp
+++ game/olc.cpp
@@ -1,6 +1,11 @@
-#include "olc.h"
+#include "olc_sdl.h"
 
 // Define our static variables
 std::atomic<bool> olcConsoleGameEngine::m_bAtomActive(false);
 std::condition_variable olcConsoleGameEngine::m_cvGameFinished;
-std::mutex olcConsoleGameEngine::m_muxGame; 
+std::mutex olcConsoleGameEngine::m_muxGame;
+
+
+int len = 0, done = 0, bits = 0, which = 0,
+sample_size = 0, position = 0, rate = 0;
+Sint16 *stream[2];
