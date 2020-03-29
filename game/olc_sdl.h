d11bc71cc522bc336489b5f6a0e2d76b04b712e3
stdmatt <stdmatt@pixelwizards.io>
2020-03-29 20:03:10 +0300

    Add the license headers

--- game/olc_sdl.h
+++ game/olc_sdl.h
@@ -1,3 +1,19 @@
+/*~---------------------------------------------------------------------------*/
+/*                        _      _                 _   _                      */
+/*                    ___| |_ __| |_ __ ___   __ _| |_| |_                    */
+/*                   / __| __/ _` | '_ ` _ \ / _` | __| __|                   */
+/*                   \__ \ || (_| | | | | | | (_| | |_| |_                    */
+/*                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   */
+/*                                                                            */
+/*  File      : olc_sdl.h                                                     */
+/*  Project   : spaceraiders                                                  */
+/*  Date      : Mar 29, 2020                                                  */
+/*  License   : GPLv3                                                         */
+/*  Author    : stdmatt <stdmatt@pixelwizards.io>                             */
+/*  Copyright : stdmatt 2020                                                  */
+/*                                                                            */
+/*  Description :                                                             */
+/*---------------------------------------------------------------------------~*/
 /*
 OneLoneCoder.com - Command Line Game Engine
 "Who needs a frame buffer?" - @Javidx9
--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -6,7 +6,7 @@
 ##                   \__ \ || (_| | | | | | | (_| | |_| |_                    ##
 ##                   |___/\__\__,_|_| |_| |_|\__,_|\__|\__|                   ##
 ##                                                                            ##
-##  File      : build_windows.ps1                                             ##
+##  File      : bump_version.sh                                               ##
 ##  Project   : spaceraiders                                                  ##
 ##  Date      : Mar 29, 2020                                                  ##
 ##  License   : GPLv3                                                         ##
@@ -16,14 +16,26 @@
 ##  Description :                                                             ##
 ##---------------------------------------------------------------------------~##
 
-$project_root=pwd;
+##----------------------------------------------------------------------------##
+## Imports                                                                    ##
+##----------------------------------------------------------------------------##
+source /usr/local/src/stdmatt/shellscript_utils/main.sh
 
-## Start the Development Console.
-Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
-Enter-VsDevShell eee160e5
 
-cd "$project_root";
+##----------------------------------------------------------------------------##
+## Variables                                                                  ##
+##----------------------------------------------------------------------------##
+SCRIPT_DIR="$(pw_get_script_dir)";
+PROJECT_ROOT=$(pw_abspath "$SCRIPT_DIR/..");
+SRC_FILE="${PROJECT_ROOT}/game/Version.h";
 
-## Compile...
-## @todo(stdmatt): Would be nice to make this output quieter...
-cl /O2 /I./libs/third_party/SDL2-2.0.12/include ./game/*.cpp ./libs/third_party/SDL2-2.0.12/lib/x86/SDL2.lib
+
+##----------------------------------------------------------------------------##
+## Script                                                                     ##
+##----------------------------------------------------------------------------##
+BUMP_THE_VERSION="$(pw_get_program_path "bump-the-version")";
+if [ -z "$BUMP_THE_VERSION" ]; then
+    pw_log_fatal "Coundn't find (bump-the-version) program - Aborting...";
+fi;
+
+"${BUMP_THE_VERSION}" "${SRC_FILE}" "#define GAME_VERSION" bump "$1";
