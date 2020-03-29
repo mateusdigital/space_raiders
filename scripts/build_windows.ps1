3a11fd5f4ae14a90e5e6707e7203690e2c5f7f72
stdmatt <stdmatt@pixelwizards.io>
2020-03-29 19:09:06 +0300

    Make the build for win32 on WSL

---
+++ scripts/build_windows.ps1
@@ -0,0 +1,11 @@
+$project_root=pwd;
+
+## Start the Development Console.
+Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll";
+Enter-VsDevShell eee160e5
+
+cd "$project_root";
+
+## Compile...
+## @todo(stdmatt): Would be nice to make this output quieter...
+cl /O2 /I./libs/third_party/SDL2-2.0.12/include -DUNICODE ./game/*.cpp ./libs/third_party/SDL2-2.0.12/lib/x86/SDL2.lib
